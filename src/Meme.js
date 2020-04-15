c9371b88860e050d76a6ed0cd39645c5864eafc4
udosinachi <udosipat@gmail.com>
2020-04-15 23:05:20 +0100

    Modified content

--- src/Meme.js
+++ src/Meme.js
@@ -4,9 +4,23 @@ class Meme extends React.Component{
     constructor(){
         super()
         this.state = {
-            
+            topText: '',
+            bottomText: '',
+            randomImg: 'http://i.imgflip.com/1bij.jng',
+            allMemeImg: []
         }
     }
+
+    componentDidMount(){
+        fetch('https://api.imgflip.com/get_memes')
+        .then(response => response.json())
+        .then(response => {
+            const {memes} = response.data
+            console.log(memes[0])
+            this.setState({allMemeImg : memes})
+        })
+    }
+
     render(){
         return(
             <h1>Meme</h1>
