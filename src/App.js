09b4ded40a5bf12279699ef7429bd4f7031c17c0
udosinachi <udosipat@gmail.com>
2020-04-15 21:46:25 +0100

    My meme generator

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Meme from './Meme'
+import Header from './Header'
+import './App.css'
+
+function App(){
+    return(
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
+
+export default App
---
+++ src/Meme.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Meme extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            
+        }
+    }
+    render(){
+        return(
+            <h1>Meme</h1>
+        )
+    }
+}
+
+export default Meme
