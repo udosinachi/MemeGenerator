8acb9133cbb98446770ff7bf9d9a08c8c1dcd83b
udosinachi <udosipat@gmail.com>
2020-04-16 20:20:20 +0100

    The meme generator withouy css

--- src/Meme.js
+++ src/Meme.js
@@ -6,9 +6,11 @@ class Meme extends React.Component{
         this.state = {
             topText: '',
             bottomText: '',
-            randomImg: 'http://i.imgflip.com/1bij.jng',
+            randomImg: 'http://i.imgflip.com/1bij.jpg',
             allMemeImg: []
         }
+        this.change = this.change.bind(this)
+        this.buttonChange = this.buttonChange.bind(this)
     }
 
     componentDidMount(){
@@ -16,14 +18,50 @@ class Meme extends React.Component{
         .then(response => response.json())
         .then(response => {
             const {memes} = response.data
-            console.log(memes[0])
             this.setState({allMemeImg : memes})
         })
     }
 
+    change(event){
+        const {name, value} = event.target
+        this.setState({ [name]: value })
+    }
+
+    buttonChange(event){
+        event.preventDefault()
+        const randNum = Math.floor(Math.random() * this.state.allMemeImg.length)
+        const randMemeImg = this.state.allMemeImg[randNum].url
+        this.setState({ randomImg : randMemeImg})
+    }
+
     render(){
         return(
-            <h1>Meme</h1>
+            <div>
+                <form className='memeform' onSubmit={this.buttonChange}>
+                    <input 
+                    type='text'
+                    name='topText'
+                    placeholder='Top Text'
+                    value={this.state.topText}
+                    onChange={this.change}
+                    />
+
+                    <input 
+                    type='text'
+                    name='bottomText'
+                    placeholder='Bottom Text'
+                    value={this.state.bottomText}
+                    onChange={this.change}
+                    />
+
+                    <button>Gen</button>
+                </form>
+                <div className='meme'>
+                    <img src={this.state.randomImg} alt='' />
+                    <h2 className='top'>{this.state.topText}</h2>
+                    <h2 className='bottom'>{this.state.bottomText}</h2>
+                </div>
+            </div>
         )
     }
 }
